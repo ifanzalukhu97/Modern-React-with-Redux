5610d31ed1b157f1b42455beeebb23f29faa684b
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-05-26 17:37:50 +0700

    Replacing Redux with Context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,29 +1,28 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
-import ColorContext from "../contexts/ColorContext"
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Kirim'
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Kirim";
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) =>  this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
-    )
+    );
   }
 
   render() {
     return (
-    <ColorContext.Consumer>
-      {(color) => this.renderButton(color)}
-    </ColorContext.Consumer>
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
-
   }
 }
 
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === "english" ? "Name" : "Nama";
+    const text = this.context.language === "english" ? "Name" : "Nama";
 
     return (
       <div className="ui field">
